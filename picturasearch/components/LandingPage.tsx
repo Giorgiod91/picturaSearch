cc2418ab7dceb53fb1c6335a9a7d21d85acda501
Giorgiod91 <giorgio.dettmar@gmx.de>
2025-11-24 19:08:57 +0100

    creating landingpage for the new project

---
+++ picturasearch/components/LandingPage.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+type Props = {};
+
+export default function LandingPage({}: Props) {
+  return (
+    <div className="flex flex-col max-w-7xl">
+      <div className="flex items-center flex-row space-x-4 mt-60">
+        <div className="flex w-1/2 flex-col s">
+          <h1 className="text-7xl p-2 text-white">picturaSearch</h1>
+          <p className="text-white p-2">
+            Dont take hours to search for the right product, we use Machine
+            Learning to anaylse from images
+          </p>
+          <button className="bg-[#9fef00] p-2 w-40 h-[60px] rounded-xl cursor-pointer ">
+            I am in
+          </button>
+        </div>
+        <div className="flex w-2/2">
+          <h1 className="text-7xl text-white"></h1>
+        </div>
+      </div>
+    </div>
+  );
+}
